abe1e572d8f8f36159c5dc67e20e9fd42bba08e4
edeotena <edeotena13@gmail.com>
2023-04-04 22:27:42 +0300

    Input fixed (without facets)

--- src/lib/s21_parse_obj_to_struct.c
+++ src/lib/s21_parse_obj_to_struct.c
@@ -1,67 +1,5 @@
 #include "s21_viewer.h"
 
-bool starts_with(const char *str, const char *start) {
-  for (; *str && *str == *start; ++str, ++start)
-    ;
-
-  return (*start) ? false : true;
-}
-
-result_code_t get_counts_from_file(obj_data *data, const char *file_name) {
-  result_code_t result_code = SUCCESS;
-
-  data->count_of_facets = 0;
-  data->count_of_vertexes = 0;
-
-  FILE *obj_file = fopen(file_name, "r");
-  if (obj_file == NULL) {
-    result_code = FILE_OPEN_ERR;
-  }
-
-  char *line = NULL;
-  size_t cap = 0;
-  while (result_code == SUCCESS && getline(&line, &cap, obj_file) != -1) {
-    if (line == NULL) {
-      result_code = FILE_READ_ERR;
-    } else if (starts_with(line, "v ") == true) {
-      ++data->count_of_vertexes;
-    } else if (starts_with(line, "f ") == true) {
-      ++data->count_of_facets;
-    } else {
-      result_code = INPUT_FORMAT_ERR;
-    }
-  }
-
-  if (line != NULL) {
-    free(line);
-  }
-
-  if (obj_file != NULL) {
-    fclose(obj_file);
-  }
-  return result_code;
-}
-
-result_code_t alloc_obj_data(obj_data *data) {
-  result_code_t result_code = SUCCESS;
-  data->coords = (coords_t *)calloc(data->count_of_vertexes, sizeof(coords_t));
-
-  if (data->coords == NULL) {
-    result_code = ALLOC_ERR;
-  }
-
-  if (result_code == SUCCESS) {
-    data->facets = (facet_t *)calloc(data->count_of_facets, sizeof(facet_t));
-
-    if (data->facets == NULL) {
-      free(data->coords);
-      result_code = ALLOC_ERR;
-    }
-  }
-
-  return result_code;
-}
-
 void skip_until_char(char **str, char ch) {
   while (**str != '\0' && **str != ch) {
     ++*str;
@@ -72,6 +10,14 @@ void skip_chars(char **str, char ch) {
   while (**str == ch) {
     ++*str;
   }
+}
+
+bool starts_with(char *str, const char *start) {
+  skip_chars(&str, ' ');
+  for (; *str && *str == *start; ++str, ++start)
+    ;
+
+  return (*start) ? false : true;
 }
 
 result_code_t get_double_and_shift(char **str, double *res) {
@@ -112,7 +58,7 @@ void replace_char(char *str, char replaceable, char replacement) {
   }
 }
 
-result_code_t get_facet(char *line, facet_t *facet, int number) {
+result_code_t read_facet_from_line(char *line, facet_t *facet, int number) {
   result_code_t result_code = SUCCESS;
   int size = 0;
 
@@ -146,30 +92,38 @@ result_code_t get_facet(char *line, facet_t *facet, int number) {
   return result_code;
 }
 
-result_code_t get_vertex(obj_data *data, char *line, int number) {
-  char *val = line + 1;
-  char *cp_line = val;
+result_code_t read_vertex_from_line(obj_data *data, char *line, int number) {
+  skip_chars(&line, ' ');
+  ++line;
+  skip_chars(&line, ' ');
 
   result_code_t result_code =
-      get_double_and_shift(&cp_line, &data->coords[number].x);
+      get_double_and_shift(&line, &data->coords[number].x);
+  skip_chars(&line, ' ');
 
   if (result_code == SUCCESS) {
-    result_code = get_double_and_shift(&cp_line, &data->coords[number].y);
+    result_code = get_double_and_shift(&line, &data->coords[number].y);
   }
+  skip_chars(&line, ' ');
 
   if (result_code == SUCCESS) {
-    result_code = get_double_and_shift(&cp_line, &data->coords[number].z);
+    result_code = get_double_and_shift(&line, &data->coords[number].z);
+  }
+  skip_chars(&line, ' ');
+
+  if (*line != '\n' && *line != '\0') {
+    result_code = INPUT_FORMAT_ERR;
   }
 
   return result_code;
 }
 
-result_code_t parse_line_to_vertexes(obj_data *data, char *line,
-                                     int *count_of_vertexes) {
+result_code_t parse_line_to_vertex(obj_data *data, char *line,
+                                   int *count_of_vertexes) {
   result_code_t result_code;
 
   if (*count_of_vertexes < data->count_of_vertexes) {
-    result_code = get_vertex(data, line, *count_of_vertexes);
+    result_code = read_vertex_from_line(data, line, *count_of_vertexes);
     ++(*count_of_vertexes);
   } else {
     result_code = WRONG_COUNT_ERR;
@@ -178,12 +132,12 @@ result_code_t parse_line_to_vertexes(obj_data *data, char *line,
   return result_code;
 }
 
-result_code_t parse_line_to_facets(obj_data *data, char *line,
-                                   int *count_of_facets) {
+result_code_t parse_line_to_facet(obj_data *data, char *line,
+                                  int *count_of_facets) {
   result_code_t result_code;
 
   if (*count_of_facets < data->count_of_facets) {
-    result_code = get_facet(line, data->facets, *count_of_facets);
+    result_code = read_facet_from_line(line, data->facets, *count_of_facets);
     if (result_code != SUCCESS) {
       for (int i = 0; i < *count_of_facets; ++i) {
         free(data->facets[i].vertexes);
@@ -193,6 +147,61 @@ result_code_t parse_line_to_facets(obj_data *data, char *line,
     ++(*count_of_facets);
   } else {
     result_code = WRONG_COUNT_ERR;
+  }
+
+  return result_code;
+}
+
+result_code_t get_counts_from_file(obj_data *data, const char *file_name) {
+  result_code_t result_code = SUCCESS;
+
+  data->count_of_facets = 0;
+  data->count_of_vertexes = 0;
+
+  FILE *obj_file = fopen(file_name, "r");
+  if (obj_file == NULL) {
+    result_code = FILE_OPEN_ERR;
+  }
+
+  char *line = NULL;
+  size_t cap = 0;
+  while (result_code == SUCCESS && getline(&line, &cap, obj_file) != -1) {
+    if (line == NULL) {
+      result_code = FILE_READ_ERR;
+    } else if (starts_with(line, "v ") == true) {
+      ++data->count_of_vertexes;
+    } else if (starts_with(line, "f ") == true) {
+      ++data->count_of_facets;
+    } else if (starts_with(line, "#") == false && *line != '\n' && *line) {
+      result_code = INPUT_FORMAT_ERR;
+    }
+  }
+
+  if (line != NULL) {
+    free(line);
+  }
+
+  if (obj_file != NULL) {
+    fclose(obj_file);
+  }
+  return result_code;
+}
+
+result_code_t alloc_obj_data(obj_data *data) {
+  result_code_t result_code = SUCCESS;
+  data->coords = (coords_t *)calloc(data->count_of_vertexes, sizeof(coords_t));
+
+  if (data->coords == NULL) {
+    result_code = ALLOC_ERR;
+  }
+
+  if (result_code == SUCCESS) {
+    data->facets = (facet_t *)calloc(data->count_of_facets, sizeof(facet_t));
+
+    if (data->facets == NULL) {
+      free(data->coords);
+      result_code = ALLOC_ERR;
+    }
   }
 
   return result_code;
@@ -214,10 +223,10 @@ result_code_t get_data_from_file(obj_data *data, const char *file_name) {
     if (line == NULL) {
       result_code = FILE_READ_ERR;
     } else if (starts_with(line, "v ") == true) {
-      result_code = parse_line_to_vertexes(data, line, &current_vertex);
+      result_code = parse_line_to_vertex(data, line, &current_vertex);
     } else if (starts_with(line, "f ") == true) {
-      result_code = parse_line_to_facets(data, line, &current_facet);
-    } else {
+      result_code = parse_line_to_facet(data, line, &current_facet);
+    } else if (starts_with(line, "#") == false && *line != '\n' && *line) {
       result_code = INPUT_FORMAT_ERR;
     }
   }
