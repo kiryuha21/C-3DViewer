18b0c99caa9eb2ac3e671d527b2d7bb3b9605db8
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-04-06 00:56:28 +0300

    transformation done, though not sure about them

--- src/lib/graphics_utils.c
+++ src/lib/graphics_utils.c
@@ -28,9 +28,14 @@ bool convert_to_double(const gchar* string, double* val) {
 void render_with_deltas(GtkWidget* widget, gpointer builder) {
   UNUSED(widget);
 
+  GtkWidget* image =
+      GTK_WIDGET(gtk_builder_get_object(builder, "visualization_image"));
+  GtkFileChooser* file_chooser =
+      GTK_FILE_CHOOSER(gtk_builder_get_object(builder, "file_selector"));
   GPtrArray* delta_data = collect_delta_data(builder);
+
   if (delta_data->len != 0) {
-    gchar* filename = gtk_file_chooser_get_filename(delta_data->pdata[4]);
+    gchar* filename = gtk_file_chooser_get_filename(file_chooser);
     if (is_null_or_empty(filename)) {
       GObject* label = gtk_builder_get_object(builder, "viewer_label");
       gtk_label_set_label(GTK_LABEL(label), MISSING_FILE_MSG);
@@ -38,34 +43,45 @@ void render_with_deltas(GtkWidget* widget, gpointer builder) {
       obj_data data;
       result_code_t result_code = s21_parse_obj_to_struct(&data, filename);
       if (result_code == SUCCESS) {
+        coords_t* scales_delta = delta_data->pdata[0];
+        s21_scale(&data, scales_delta);
+
+        coords_t* coords_delta = delta_data->pdata[1];
+        s21_move(&data, coords_delta);
+
+        coords_t* angles_delta = delta_data->pdata[2];
+        s21_rotate(&data, angles_delta);
+
         s21_write_coords_to_file(&data, POINTS_FILE);
-        gnuplot_call_wrapper(delta_data->pdata[3]);
+        gnuplot_call_wrapper(image);
         s21_free_obj_data(&data);
       }
     }
-
     g_free(filename);
   }
-  g_ptr_array_free(delta_data, false);
+  g_ptr_array_free(delta_data, true);
 }
 
 GPtrArray* collect_delta_data(GtkBuilder* builder) {
   GPtrArray* delta_data = g_ptr_array_new();
 
-  double scale_delta = 0;
-  coords_t coords = {0, 0, 0}, angles = {0, 0, 0};
+  coords_t* coords = calloc(1, sizeof(coords_t));
+  coords_t* angles = calloc(1, sizeof(coords_t));
+  coords_t* scales = calloc(1, sizeof(coords_t));
 
-  GObject* scale_entry = gtk_builder_get_object(builder, "scale_entry");
+  GObject* x_scale = gtk_builder_get_object(builder, "x_scale_delta_entry");
+  GObject* y_scale = gtk_builder_get_object(builder, "y_scale_delta_entry");
+  GObject* z_scale = gtk_builder_get_object(builder, "z_scale_delta_entry");
   GObject* x_coord = gtk_builder_get_object(builder, "x_coord_delta_entry");
   GObject* y_coord = gtk_builder_get_object(builder, "y_coord_delta_entry");
   GObject* z_coord = gtk_builder_get_object(builder, "z_coord_delta_entry");
   GObject* x_angle = gtk_builder_get_object(builder, "x_angle_delta_entry");
   GObject* y_angle = gtk_builder_get_object(builder, "y_angle_delta_entry");
   GObject* z_angle = gtk_builder_get_object(builder, "z_angle_delta_entry");
-  GObject* image = gtk_builder_get_object(builder, "visualization_image");
-  GObject* file_chooser = gtk_builder_get_object(builder, "file_selector");
 
-  const gchar* scale_text = gtk_entry_get_text(GTK_ENTRY(scale_entry));
+  const gchar* x_scale_text = gtk_entry_get_text(GTK_ENTRY(x_scale));
+  const gchar* y_scale_text = gtk_entry_get_text(GTK_ENTRY(y_scale));
+  const gchar* z_scale_text = gtk_entry_get_text(GTK_ENTRY(z_scale));
   const gchar* x_coord_text = gtk_entry_get_text(GTK_ENTRY(x_coord));
   const gchar* y_coord_text = gtk_entry_get_text(GTK_ENTRY(y_coord));
   const gchar* z_coord_text = gtk_entry_get_text(GTK_ENTRY(z_coord));
@@ -73,21 +89,21 @@ GPtrArray* collect_delta_data(GtkBuilder* builder) {
   const gchar* y_angle_text = gtk_entry_get_text(GTK_ENTRY(y_angle));
   const gchar* z_angle_text = gtk_entry_get_text(GTK_ENTRY(z_angle));
 
-  if (convert_to_double(scale_text, &scale_delta) == false ||
-      convert_to_double(x_coord_text, &coords.x) == false ||
-      convert_to_double(y_coord_text, &coords.y) == false ||
-      convert_to_double(z_coord_text, &coords.z) == false ||
-      convert_to_double(x_angle_text, &angles.x) == false ||
-      convert_to_double(y_angle_text, &angles.y) == false ||
-      convert_to_double(z_angle_text, &angles.z) == false) {
+  if (convert_to_double(x_scale_text, &scales->x) == false ||
+      convert_to_double(y_scale_text, &scales->y) == false ||
+      convert_to_double(z_scale_text, &scales->z) == false ||
+      convert_to_double(x_coord_text, &coords->x) == false ||
+      convert_to_double(y_coord_text, &coords->y) == false ||
+      convert_to_double(z_coord_text, &coords->z) == false ||
+      convert_to_double(x_angle_text, &angles->x) == false ||
+      convert_to_double(y_angle_text, &angles->y) == false ||
+      convert_to_double(z_angle_text, &angles->z) == false) {
     GObject* label = gtk_builder_get_object(builder, "viewer_label");
     gtk_label_set_label(GTK_LABEL(label), WRONG_INPUT_MSG);
   } else {
-    g_ptr_array_add(delta_data, &scale_delta);
-    g_ptr_array_add(delta_data, &coords);
-    g_ptr_array_add(delta_data, &angles);
-    g_ptr_array_add(delta_data, GTK_WIDGET(image));
-    g_ptr_array_add(delta_data, GTK_FILE_CHOOSER(file_chooser));
+    g_ptr_array_add(delta_data, scales);
+    g_ptr_array_add(delta_data, coords);
+    g_ptr_array_add(delta_data, angles);
   }
 
   return delta_data;
