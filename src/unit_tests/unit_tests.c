6bf60455c47b64ed2bb051c816308f349473598e
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-04-06 14:40:05 +0300

    template for unit tests

--- src/unit_tests/unit_tests.c
+++ src/unit_tests/unit_tests.c
@@ -1,1 +1,16 @@
 #include "unit_tests.h"
+
+int main() {
+  setlocale(LC_NUMERIC, "ru_RU.UTF-8");
+  Suite* suites[SUITES_COUNT] = {
+      get_affine_transformations_suite(), get_deallocation_suite(),
+      get_object_reading_suite(), get_object_writing_suite()};
+
+  for (int i = 0; i < SUITES_COUNT; ++i) {
+    SRunner* srunner = srunner_create(suites[i]);
+    srunner_run_all(srunner, CK_NORMAL);
+    srunner_free(srunner);
+  }
+
+  return 0;
+}
--- src/unit_tests/unit_tests.h
+++ src/unit_tests/unit_tests.h
@@ -1,4 +1,15 @@
 #ifndef C8_3DVIEWER_V1_0_1_UNIT_TESTS_H
 #define C8_3DVIEWER_V1_0_1_UNIT_TESTS_H
 
+#include <check.h>
+
+#include "../s21_viewer.h"
+
+#define SUITES_COUNT 4
+
+Suite* get_affine_transformations_suite();
+Suite* get_deallocation_suite();
+Suite* get_object_reading_suite();
+Suite* get_object_writing_suite();
+
 #endif  // C8_3DVIEWER_V1_0_1_UNIT_TESTS_H
